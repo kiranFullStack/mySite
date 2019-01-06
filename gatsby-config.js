a85e71486229ee350636cd399cd9c8140dac8b34
APPLE <apple@APPLEs-MacBook-Air.local>
2019-01-06 20:40:29 +0530

    all test commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Kiran Full Stack`,
+    title: `Kiran Full Stack 1`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
